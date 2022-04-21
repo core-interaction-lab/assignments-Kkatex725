9870e2fbbd6da17681d6dfd7968734c1a790d229
Kkatex725 <xianq734@newschool.edu>
2022-04-21 17:07:31 -0400

    1

---
+++ assignments/assignment-5/main.js
@@ -0,0 +1,7 @@
+const fetchContent = async () => {
+    const response = await fetch('https://interactionlab.space/data/assignment-4-1.json').then(data => data.json());
+console.log(response);
+
+};
+
+fetchContent();
